d44678fada6f902b53f94f21fc96be5aefd5a64e
@willhead <60652390+will-head@users.noreply.github.com>
2020-05-04 15:42:08 +0100

    Refactors Bookmark implentation to use CONST for bookmarks and references CONST in test

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,10 +1,12 @@
 class Bookmark
+  TEST_BOOKMARKS = ["Bookmark 1", "Bookmark 2"]
 
   def all
-    bookmarks = Array.new
-    bookmarks.push("Bookmark 1")
-    bookmarks.push("Bookmark 2")
-    bookmarks
+    # bookmarks = ["Bookmark 1", "Bookmark 2"]
+    # # bookmarks.push("Bookmark 1")
+    # # bookmarks.push("Bookmark 2")
+    # bookmarks
+    TEST_BOOKMARKS
   end
 
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,10 +5,7 @@ describe Bookmark do
   describe '#all' do
 
     it 'returns the bookmarks' do
-      bookmarks = Array.new
-      bookmarks.push("Bookmark 1")
-      bookmarks.push("Bookmark 2")
-      expect(subject.all).to eq bookmarks
+      expect(subject.all).to eq Bookmark::TEST_BOOKMARKS
     end
 
   end
